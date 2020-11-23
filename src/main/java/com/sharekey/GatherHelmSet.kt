a122c794c104eb8a6ebfe45157b0845029e5a364
Viachaslau Tsypylou <slava.tsypylov@sharekey.com>
2020-11-23 15:54:00 +0300

    add prefix to params of the function

--- src/main/java/com/sharekey/GatherHelmSet.kt
+++ src/main/java/com/sharekey/GatherHelmSet.kt
@@ -2,11 +2,10 @@ package com.sharekey
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.Parameter
 
-const val helmSetParamPrefix = "helm.set."
-fun GatherHelmSet(params: List<Parameter>): List<String> {
+fun GatherHelmSet(prefix: String, params: List<Parameter>): List<String> {
     val result = mutableListOf<String>()
     for (param in params) {
-        if (param.name.startsWith(helmSetParamPrefix)){
+        if (param.name.startsWith(prefix)){
             result.add("--set \"%${param.name}%\"")
         }
     }

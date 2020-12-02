c5d70696028c5e5a97474073c283134a2a361814
Daniel Chaplin <black.danek@gmail.com>
2020-12-03 00:34:13 +0300

    refactor GatherHelmSetParameters func with default prefix parameter

--- src/main/java/com/sharekey/GatherHelmSetParameters.kt
+++ src/main/java/com/sharekey/GatherHelmSetParameters.kt
@@ -2,7 +2,7 @@ package com.sharekey
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.Parameter
 
-fun GatherHelmSetParameters(prefix: String, params: List<Parameter>): List<String> {
+fun GatherHelmSetParameters(params: List<Parameter>, prefix: String = HelmSetParamPrefix): List<String> {
     val result = mutableListOf<String>()
     for (param in params) {
         if (param.name.startsWith(prefix) && param.spec != ParameterPassword){
---
+++ src/main/java/com/sharekey/HelmSetParamPrefix.kt
@@ -0,0 +1,3 @@
+package com.sharekey
+
+const val HelmSetParamPrefix = "helm.set."
